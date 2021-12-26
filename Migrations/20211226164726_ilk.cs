741c0e12a33117b2f91792e13248f71b0a4718ce
mertsigirci11 <62725758+mertsigirci11@users.noreply.github.com>
2021-12-26 23:30:37 +0300

    Add files via upload

---
+++ Migrations/20211226164726_ilk.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Migrations
+{
+    public partial class ilk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adminler",
+                columns: table => new
+                {
+                    AdminAd = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AdminSifre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adminler", x => x.AdminAd);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doktorlar",
+                columns: table => new
+                {
+                    DoktorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoktorAd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoktorSoyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoktorSifre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Aktiflik = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doktorlar", x => x.DoktorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hastalar",
+                columns: table => new
+                {
+                    HastaTCNo = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HastaAd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HastaSoyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HastaSifre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hastalar", x => x.HastaTCNo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Randevular",
+                columns: table => new
+                {
+                    RandevuID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HastaTCNo = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DoktorID = table.Column<int>(type: "int", nullable: false),
+                    Tarih = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RandevuSaati = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Randevular", x => x.RandevuID);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Doktorlar_DoktorID",
+                        column: x => x.DoktorID,
+                        principalTable: "Doktorlar",
+                        principalColumn: "DoktorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Hastalar_HastaTCNo",
+                        column: x => x.HastaTCNo,
+                        principalTable: "Hastalar",
+                        principalColumn: "HastaTCNo",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_DoktorID",
+                table: "Randevular",
+                column: "DoktorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_HastaTCNo",
+                table: "Randevular",
+                column: "HastaTCNo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adminler");
+
+            migrationBuilder.DropTable(
+                name: "Randevular");
+
+            migrationBuilder.DropTable(
+                name: "Doktorlar");
+
+            migrationBuilder.DropTable(
+                name: "Hastalar");
+        }
+    }
+}
