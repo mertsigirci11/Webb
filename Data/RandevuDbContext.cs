83b9aaf3acb95865f8a825fcfffe0491c3de64df
mertsigirci11 <62725758+mertsigirci11@users.noreply.github.com>
2021-12-26 23:30:25 +0300

    Add files via upload

---
+++ Data/RandevuDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Randevu_Sistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Randevu_Sistemi.Data
+{
+    public class RandevuDbContext : DbContext
+    {
+        public RandevuDbContext(DbContextOptions<RandevuDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Hasta> Hastalar { get; set; }
+
+        public DbSet<Doktor> Doktorlar { get; set; }
+
+        public DbSet<Admin> Adminler{ get; set; }
+
+        public DbSet<Randevu> Randevular { get; set; }
+    }
+}
